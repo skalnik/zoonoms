beb33e5c9a56593bc4fe98cf787349c2defab1c5
Mike Skalnik <mike.skalnik@gmail.com>
2011-02-20 13:45:11 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zoonoms::Application.config.secret_token = '0af69eadb411d09378d2cf32cfd8ba4e22bdec7c2d3326341044bc49d9cdfe72902ca945739270f1f4c73e793fa371c19a045cdbe8210c8704cb11f1103ea896'
