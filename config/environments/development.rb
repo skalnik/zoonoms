e7394b565ab0e856888a2740bdec3211257c9a56
Mike Skalnik <mike.skalnik@gmail.com>
2011-02-23 11:27:18 -0500

    Cleaned development.rb & production.rb and added default URL for mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Zoonoms::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
-
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -16,6 +14,8 @@ Zoonoms::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  # Set correct URL for mailer
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
